66ded0f19f1d3cbfdc0f768f4ee500b1323d579e
Bidur Sahoo <78256127+Bidursahoo@users.noreply.github.com>
2024-10-16 19:26:26 +0530

    Added css

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,7 +1,29 @@
+import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import { ArticleData } from "@/types/newsArticle";
+
 export default function Page({ params }: { params: { slug: string } }) {
+  const newsDetails = DUMMY_NEWS.find(
+    (newsItem) => newsItem.slug === params.slug
+  ) as ArticleData | undefined;
+  if (!newsDetails) {
+    return notFound();
+  }
+
   return (
-    <div>
-      <h1>{params.slug}</h1>
-    </div>
+    <article className="news-article">
+      <header>
+        <Image
+          src={`/images/news/${newsDetails.image}`}
+          alt={newsDetails.title}
+          width={100}
+          height={100}
+        />
+        <h1>{newsDetails.title}</h1>
+        <time dateTime={newsDetails.date}>{newsDetails.date}</time>
+      </header>
+      <p>{newsDetails.content}</p>
+    </article>
   );
 }
--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,20 +1,27 @@
+import { DUMMY_NEWS } from "@/dummy-news";
 import Link from "next/link";
-
+import Image from "next/image";
 export default function NewsPage() {
   return (
-    <div>
+    <>
       <h1>News Page</h1>
-      <ul>
-        <li>
-          <Link href="/news/firstPage">first news page </Link>
-        </li>
-        <li>
-          <Link href="/news/secondPage">second news page </Link>
-        </li>
-        <li>
-          <Link href="/news/thirdPage">third news page </Link>
-        </li>
+      <ul className="news-list">
+        {DUMMY_NEWS.map((newsItems) => {
+          return (
+            <li key={newsItems.id}>
+              <Link href={`/news/${newsItems.slug}`}>
+                <Image
+                  src={`/images/news/${newsItems.image}`}
+                  alt={newsItems.title}
+                  width={100}
+                  height={100}
+                />
+                <span>{newsItems.title}</span>
+              </Link>
+            </li>
+          );
+        })}
       </ul>
-    </div>
+    </>
   );
 }

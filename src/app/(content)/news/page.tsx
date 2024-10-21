6c4a7393bac4b4f093d388245578b105094386b6
Bidur Sahoo <78256127+Bidursahoo@users.noreply.github.com>
2024-10-21 10:03:40 +0530

    clinet side data fetching

--- src/app/(content)/news/page.tsx
+++ src/app/(content)/news/page.tsx
@@ -1,11 +1,53 @@
+// import NewsList from "@/components/News/News";
+// import { DUMMY_NEWS } from "@/dummy-news";
+
+// export default function NewsPage() {
+//   return (
+//     <>
+//       <h1>News Page</h1>
+//       <NewsList news={DUMMY_NEWS} />
+//     </>
+//   );
+// }
+// client side updation
+
+"use client";
 import NewsList from "@/components/News/News";
-import { DUMMY_NEWS } from "@/dummy-news";
+// import { DUMMY_NEWS } from "@/dummy-news";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorState, setErrorState] = useState(undefined);
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const newsResponse = await fetch("http://localhost:8080/news");
+      if (!newsResponse.ok) {
+        setErrorState("Error Fetching Data");
+        setIsLoading(false);
+      }
+      setIsLoading(false);
+      const news = await newsResponse.json();
+      setNews(news);
+    }
+    fetchNews();
+  }, []);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (errorState) {
+    return <p>{errorState}</p>;
+  }
+  let newsContent;
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
